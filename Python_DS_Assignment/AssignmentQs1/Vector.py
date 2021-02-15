69c59ef05f287620605939ab77496d799de88030
Nisarg Upadhyaya <nisarg1631@gmail.com>
2021-02-15 15:26:58 +0530

    SE python ds asgn

--- Python_DS_Assignment/AssignmentQs1/Vector.py
+++ Python_DS_Assignment/AssignmentQs1/Vector.py
@@ -1,55 +1,109 @@
-# In this python file, only the definations for the magic functions and the basic operations
-# for the question segments are provided. There may be the need to add new functions or overload 
-# existing ones as per the question requirements.
-
 class Vector:
-        
-    def __init__(self, *args): 
 
-        # if arg is an int(dimension)
-        if isinstance(args[0], int): 
-            self._coords = [0]*args[0]
+    def __init__(self, *args):
+        self._coords = []
+        if isinstance(args[0], int):
+            self._coords = [0] * args[0]
+        else:
+            try:
+                for x in args[0]:
+                    if not isinstance(x, int):
+                        raise Exception("Iterable should contain only 'int'")
+                    self._coords.append(x)
+            except Exception as what:
+                print(what)
 
     def __len__(self):
-        # return the dimension of the vector
+        return len(self._coords)
 
-    def __getitem__(self, j):
-        # return the jth coordinate of the vector
+    def __getitem__(self, j):  # indexing from 1
+        return self._coords[j - 1]
 
-    def __setitem__(self, j, val):
-        # set the jth coordinate of vector to val
+    def __setitem__(self, j, val):  # indexing from 1
+        self._coords[j - 1] = val
 
     def __add__(self, other):
-        # u + v
-            
+        try:
+            if len(self) == len(other):
+                return Vector([x + y for (x, y) in zip(self._coords, other._coords)])
+            else:
+                raise Exception("Length error.")
+        except Exception as what:
+            print(what)
+
     def __eq__(self, other):
-        # return True if vector has same coordinates as other
-    
+        return self._coords == other._coords
+
     def __ne__(self, other):
-        # return True if vector differs from other
-    
+        return not self == other
+
     def __str__(self):
-        # return the string representation of a vector within <>
+        if not len(self):
+            return "<>"
+        vec = f"<{self._coords[0]}"
+        for coord in self._coords[1:]:
+            vec = vec + f", {coord}"
+        vec = vec + ">"
+        return vec
 
     def __sub__(self, other):
-        # Soln for Qs. 2
-    
-    def __neg__(self):
-        # Soln for Qs. 3
-    
-    def __rmul__(self, value):
-        return (self * value) 
-    
-    def __mul__(self, other):
-        # Soln for Qs. 4, 5 and 6
-    
-def main():
-    v1 = Vector(5)
-    v2 = Vector (7)
-    v3 = Vector([1,2,3,4,5])
+        return self + (-other)
 
-    # Add suitable print statements to display the results
-    # of the different question segments
+    def __neg__(self):
+        return Vector([-x for x in self._coords])
+
+    def __rmul__(self, value):
+        return self * value
+
+    def __mul__(self, other):
+        try:
+            if isinstance(other, int):
+                return Vector([other * x for x in self._coords])
+            elif isinstance(other, Vector):
+                if len(self) == len(other):
+                    return sum([u * v for (u, v) in zip(self._coords, other._coords)])
+                else:
+                    raise Exception("Length error.")
+            else:
+                raise Exception(f"Cannot multiply objects of type {type(self).__name__} and {type(other).__name__}")
+        except Exception as what:
+            print(what)
+
+
+def main():
+    v1 = Vector(5)  # construction from integer
+    v2 = Vector(7)  # construction from integers
+    v3 = Vector([1, 2, 3, 4, 5])  # construction from iterable
+    v4 = Vector((x % 2 + 1 for x in range(5)))  # construction from iterable
+    temp_dict = {'a': 1, 'b': 2, 'c': 3, 'd': 4, 'e': 5}
+    v5 = Vector(temp_dict.values())  # construction from iterable
+
+    print(f"v1 = {v1}")
+    print(f"v2 = {v2}")
+    print(f"v3 = {v3}")
+    print(f"v4 = {v4}")
+    print(f"v5 = {v5}")
+    print()
+    print(f"Length of v2 = {len(v2)}")
+    print(f"Length of v5 = {len(v5)}")
+    print()
+    print(f"v4[4] = {v4[4]}")
+    print("Setting v2[5] to 9")
+    v2[5] = 9
+    print(f"v2 = {v2}")
+    print()
+    print(f"v1 == v2 -> {v1==v2}")
+    print(f"v3 == v5 -> {v3 == v5}")
+    print(f"v3 != v4 -> {v3 != v4}")
+    print(f"v3 != v5 -> {v3 != v5}")
+    print()
+    print(f"v4 + v3 = {v4 + v3}")
+    print(f"v4 - v3 = {v4 - v3}")
+    print(f"-v4 = {-v4}")
+    print()
+    print(f"v3 * 3 = {v3 * 3}")
+    print(f"3 * v3 = {3 * v3}")
+    print(f"v3 * v4 = {v3 * v4}")
 
 
 if __name__ == '__main__':
--- Python_DS_Assignment/AssignmentQs2/main.py
+++ Python_DS_Assignment/AssignmentQs2/main.py
@@ -1,11 +1,12 @@
-#Imports
+# Imports
 from my_package.model import ObjectDetectionModel
 from my_package.data import Dataset
-from my_package.analysis import show_boxes
+from my_package.analysis import plot_boxes
 from my_package.data.transforms import FlipImage, RescaleImage, BlurImage, CropImage, RotateImage
 
+
 def experiment(annotation_file, detector, transforms, outputs):
-    '''
+    """
         Function to perform the desired experiments
 
         Arguments:
@@ -13,28 +14,23 @@ def experiment(annotation_file, detector, transforms, outputs):
         detector: The object detector
         transforms: List of transformation classes
         outputs: path of the output folder to store the images
-    '''
+    """
 
-    #Create the instance of the dataset.
-    
+    # Create the instance of the dataset.
 
-    #Iterate over all data items.
-    
+    # Iterate over all data items.
 
-    #Get the predictions from the detector.
-    
+    # Get the predictions from the detector.
 
-    #Draw the boxes on the image and save them.
+    # Draw the boxes on the image and save them.
 
-
-    #Do the required analysis experiments.
-    
+    # Do the required analysis experiments.
 
 
 def main():
     detector = ObjectDetectionModel()
-    experiment('./data/annotations.jsonl', detector, [FlipImage(), BlurImage()], None) # Sample arguments to call experiment()
-
+    experiment('./data/annotations.jsonl', detector, [FlipImage(), BlurImage()],
+               None)  # Sample arguments to call experiment()
 
 
 if __name__ == '__main__':
