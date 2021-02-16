f486b90e479cec01af14cfcd5ed0f355a2ba4b91
Nisarg Upadhyaya <nisarg1631@gmail.com>
2021-02-16 13:58:04 +0530

    done! only package creation left

--- Python_DS_Assignment/AssignmentQs2/my_package/analysis/visualize.py
+++ Python_DS_Assignment/AssignmentQs2/my_package/analysis/visualize.py
@@ -10,6 +10,7 @@ def plot_boxes(img, bboxes, labels, output):  # Write the required arguments
     for i in range(min(5, len(bboxes))):
         ImageDraw.Draw(img).text(bboxes[i][0], labels[i], fill='yellow'
                                  , font=ImageFont.truetype('/usr/share/fonts/truetype/ubuntu/UbuntuMono-R.ttf', size=20)
+                                 # uncomment above line to set custom font from system
                                  )
     img.save(output)
 # The function should plot the predicted boxes on the images and save them.
